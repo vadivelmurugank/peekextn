74648753901dd238db42d041f6788ea2eef0979c
vad <vadivelmurugank@gmail.com>
2015-10-21 04:44:56 +0000

    Add packages to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup
 
 setup(
   name = 'peekextn',
-  version = '0.1',
+  packages_dir = {'peekextn': 'src'},
+  packages = ['peekextn'],
+  version = '0.3',
   description = 'Peek extension and show respective sources',
   author = 'Vadivel',
   author_email = 'vadivelmurugank@gmail.com',
