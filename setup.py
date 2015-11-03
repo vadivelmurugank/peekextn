0a0a45bb11dcca571ef7357fe6effaef4ab75041
vad <vadivelmurugank@gmail.com>
2015-11-03 22:09:37 +0000

    update version 0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   name = 'peekextn',
   packages_dir = {'peekextn': 'src'},
   packages = ['peekextn'],
-  version = '0.5',
+  version = '0.7',
   description = 'Peek extension and show respective sources',
   author = 'Vadivel',
   author_email = 'vadivelmurugank@gmail.com',
